9a3f65ef3a4fd581e4191425904e392c9aa7fb87
MatheusAssis <matheusassismelo@gmail.com>
2022-08-13 01:48:12 -0300

    add function to get teams by id from football api

--- be_challenge_massis/services/football_api.py
+++ be_challenge_massis/services/football_api.py
@@ -1,6 +1,7 @@
+import os
+
 import requests
 from fastapi import HTTPException
-import os
 
 FOOTBALL_API_URL = os.getenv("FOOTBALL_API_URL")
 FOOTBALL_API_KEY = os.getenv("FOOTBALL_API_KEY")
@@ -19,6 +20,17 @@ class FootballAPIService:
                 status_code=result.status_code, detail="Impossible to get this league"
             )
         return result.json()
+
+    def get_teams(self, team_ids: list):
+        teams = []
+        for team_id in team_ids:
+            result = requests.get(
+                f"{FOOTBALL_API_URL}/teams/{team_id}", headers=self._headers
+            )
+            if result.status_code != 200:
+                continue
+            teams.append(result.json())
+        return teams
 
     def _set_token_header(self):
         return {"X-Auth-Token": FOOTBALL_API_KEY}
