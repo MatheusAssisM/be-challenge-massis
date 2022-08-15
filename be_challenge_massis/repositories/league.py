cd8f6525ee3e0c2e213b7a21ec6b972bb1b00e6e
MatheusAssis <matheusassismelo@gmail.com>
2022-08-14 22:05:31 -0300

    update repositories to create league and teams associations

--- be_challenge_massis/repositories/league.py
+++ be_challenge_massis/repositories/league.py
@@ -1,19 +1,30 @@
+from typing import List
 from sqlalchemy.orm import Session
+
+from ..models.team import Team
 from ..models.league import League
 
 
 class LeagueRepository:
     def __init__(self, session_db: Session):
         self.session_db = session_db
-        self.league = League
 
     def create(self, league_data: dict):
-        league = self.league(**league_data)
+        league = League(**league_data)
         with self.session_db() as session:
             session.add(league)
             session.commit()
+            session.refresh(league)
         return league
+
+    def associate_team_league(self, league: League, teams: List[Team]):
+        for team in teams:
+            league.teams.append(team)
+
+        with self.session_db() as session:
+            session.add(league)
+            session.commit()
 
     def get_by_code(self, league_code: str):
         with self.session_db() as session:
-            return session.query(self.league).filter_by(code=league_code).first()
+            return session.query(League).filter_by(code=league_code).first()
--- be_challenge_massis/repositories/team.py
+++ be_challenge_massis/repositories/team.py
@@ -1,16 +1,16 @@
+from typing import List
 from sqlalchemy.orm import Session
+
 from ..models.team import Team
 
 
 class TeamRepository:
     def __init__(self, session_db: Session):
         self.session_db = session_db
-        self.team = Team
 
-    def bulk_create(self, team_data_list: list):
-        teams = [self.team(**team) for team in team_data_list]
+    def bulk_create(self, team_data_list: List[dict]):
+        teams = [Team(**team) for team in team_data_list]
         with self.session_db() as session:
-            session.bulk_save_objects(teams)
+            session.bulk_save_objects(teams, return_defaults=True)
             session.commit()
         return teams
-
